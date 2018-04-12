4e748b969c5d447c994e5cfdf6f265c5664db8a0
Edoardo Ceccarelli <ridleys@gmail.com>
2018-04-12 09:47:25 +0300

    small script to generate 100 test records

--- generateData.sh
+++ generateData.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cnt=12
+cnt=0
 echo cont = $cnt
-echo "insert into cibucks.campaigns (PK,key,profile) values($((++cnt)),1,'JSON[{\"interestIds\":[1, 2], \"groupId\":1}, {\"interestIds\":[3], \"groupId\":2}]')"
-echo 'insert into cibucks.campaigns (PK,key,profile) values(2,2,'JSON[{"interestIds":[11, 21], "groupId":22}, {"interestIds":[3, 45, 32], "groupId":3}]')'
-echo 'insert into cibucks.campaigns (PK,key,profile) values(3,3,'JSON[{"interestIds":[23], "groupId":1}, {"interestIds":[3], "groupId":2}]')'
-echo 'insert into cibucks.campaigns (PK,key,profile) values(4,4,'JSON[{"interestIds":[1, 2], "groupId":1}, {"interestIds":[4,5], "groupId":2}]')'
+for i in {1..25}
+do
+    echo "insert into cibucks.campaigns (PK,key,profile) values($((++cnt)),$((cnt)),'JSON[{\"interestIds\":[1, 2], \"groupId\":1}, {\"interestIds\":[3], \"groupId\":2}]')"
+    echo "insert into cibucks.campaigns (PK,key,profile) values($((++cnt)),$((cnt)),'JSON[{\"interestIds\":[11, 21], \"groupId\":22}, {\"interestIds\":[3, 45, 32], \"groupId\":3}]')"
+    echo "insert into cibucks.campaigns (PK,key,profile) values($((++cnt)),$((cnt)),'JSON[{\"interestIds\":[23], \"groupId\":1}, {\"interestIds\":[3], \"groupId\":2}]')"
+    echo "insert into cibucks.campaigns (PK,key,profile) values($((++cnt)),$((cnt)),'JSON[{\"interestIds\":[1, 2], \"groupId\":1}, {\"interestIds\":[4, 5], \"groupId\":2}]')"
+done
+
